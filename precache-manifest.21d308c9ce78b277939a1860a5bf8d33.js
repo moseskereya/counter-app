7169353f400645669ac74572ebb1525af995ef0f
MOSES <MOSESLAZARO2019@outlook.com>
2019-08-30 07:01:16 +0300

    Updates

---
+++ precache-manifest.21d308c9ce78b277939a1860a5bf8d33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9055feae85d2fea26698b3d26466758",
+    "url": "https//moseskereya.github.io/counter-app/index.html"
+  },
+  {
+    "revision": "0efe8535ec1457135f0d",
+    "url": "https//moseskereya.github.io/counter-app/static/css/main.f3031638.chunk.css"
+  },
+  {
+    "revision": "2b5643e757cd35ca5d8a",
+    "url": "https//moseskereya.github.io/counter-app/static/js/2.49a21540.chunk.js"
+  },
+  {
+    "revision": "0efe8535ec1457135f0d",
+    "url": "https//moseskereya.github.io/counter-app/static/js/main.3bf00c90.chunk.js"
+  },
+  {
+    "revision": "ae90f26185e00156e697",
+    "url": "https//moseskereya.github.io/counter-app/static/js/runtime~main.ed2eca9f.js"
+  }
+]);
